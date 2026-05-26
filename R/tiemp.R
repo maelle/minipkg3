ed44039867859c12a0acabebf02bcde499fac09d
Maëlle Salmon <maelle.salmon@yahoo.se>
2026-05-26 15:25:06 +0200

    roxygen2

---
+++ R/tiemp.R
@@ -0,0 +1,23 @@
+#' Da la hora
+#'
+#' @param language Idioma, o "es" o "en"
+#'
+#' @returns Una frase
+#'
+#' @export
+#' @examples
+#' what_time()
+#' what_time("en")
+what_time <- function(language = "es") {
+  if (!language %in% c("es", "en")) {
+    stop("Either choose 'es' or 'en' as a language.")
+  }
+
+  time <- format(Sys.time(), "%H:%M")
+
+  switch(
+    language,
+    es = sprintf("¡Ahora son las %s!", time),
+    en = sprintf("It is %s now!", time)
+  )
+}
